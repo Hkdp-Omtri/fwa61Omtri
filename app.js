6f2de153873d626c8052b4da0dfacc440bc665f3
Homesh_omtri <xyxyx@gmail.com>
2021-10-26 16:26:21 -0500

    adding files

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/mydata');
+var mydatRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/mydata', mydataRouter);
+app.use('/users', usersRouter);
+app.use('/mydata',mydatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mydata.js
+++ mydata.js
@@ -1,41 +1,9 @@
-var createError = require('http-errors');
-var express = require('HOMAKESAVADURGAPRASAD OMTRI');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express');
+var router = express.Router();
 
-var indexRouter = require('./routes/in99ikdex');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'HOMAKESAVADURGAPRASAD' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = router;
