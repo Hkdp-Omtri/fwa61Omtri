dccb3c350fb70ee8aa85babc67e5b698262050ab
Homesh_omtri <xyxyx@gmail.com>
2021-10-26 22:45:56 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydatRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 var indexRouter = require('./routes/index');
-
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata',mydatRouter);
+app.use('/mydata',mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
